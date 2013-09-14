b81d90317f8da03f23731c2240a16580259b734e
Torrentula <orwar@web.de>
2013-09-14 11:14:11 +0200

    removed a lot of unneded comments

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/* Pin connections
+ * USART1_TX: PB6; USART1_RX: PB7
+ * USART2_TX: PA2
+ * I2C1_SDA: PB9; I2C1_SCL: PB8
+ */
+
 #include <stm32f4xx.h>
 #include <misc.h>			 // I recommend you have a look at these in the ST firmware folder
 #include <stm32f4xx_usart.h> // under Libraries/STM32F4xx_StdPeriph_Driver/inc and src
@@ -35,44 +41,10 @@ void Delay(__IO uint32_t nCount) {
  * them as inputs and outputs 
  */
 void GPIO_init(void){
-	
-	/* This TypeDef is a structure defined in the
-	 * ST's library and it contains all the properties
-	 * the corresponding peripheral has, such as output mode,
-	 * pullup / pulldown resistors etc.
-	 * 
-	 * These structures are defined for every peripheral so 
-	 * every peripheral has it's own TypeDef. The good news is
-	 * they always work the same so once you've got a hang
-	 * of it you can initialize any peripheral.
-	 * 
-	 * The properties of the periperals can be found in the corresponding
-	 * header file e.g. stm32f4xx_gpio.h and the source file stm32f4xx_gpio.c
-	 */
 	GPIO_InitTypeDef GPIO_InitStruct;
 	
-	/* This enables the peripheral clock to the GPIOD IO module
-	 * Every peripheral's clock has to be enabled 
-	 * 
-	 * The STM32F4 Discovery's User Manual and the STM32F407VGT6's
-	 * datasheet contain the information which peripheral clock has to be used.
-	 * 
-	 * It is also mentioned at the beginning of the peripheral library's 
-	 * source file, e.g. stm32f4xx_gpio.c
-	 */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	
-	/* In this block of instructions all the properties
-	 * of the peripheral, the GPIO port in this case,
-	 * are filled with actual information and then 
-	 * given to the Init function which takes care of 
-	 * the low level stuff (setting the correct bits in the 
-	 * peripheral's control register)
-	 * 
-	 * 
-	 * The LEDs on the STM324F Discovery are connected to the
-	 * pins PD12 thru PD15
-	 */
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12; // we want to configure all LED GPIO pins
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT; 		// we want the pins to be an output
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; 	// this sets the GPIO modules clock speed
@@ -80,16 +52,8 @@ void GPIO_init(void){
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL; 	// this sets the pullup / pulldown resistors to be inactive
 	GPIO_Init(GPIOD, &GPIO_InitStruct); 			// this finally passes all the values to the GPIO_Init function which takes care of setting the corresponding bits.
 	
-	/* This enables the peripheral clock to 
-	 * the GPIOA IO module
-	 */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	
-	/* Here the GPIOA module is initialized.
-	 * We want to use PA0 as an input because
-	 * the USER button on the board is connected
-	 * between this pin and VCC.
-	 */
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;		  // we want to configure PA0
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN; 	  // we want it to be an input
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;//this sets the GPIO modules clock speed
@@ -104,33 +68,14 @@ void GPIO_init(void){
  * 						   supposed to operate
  */
 void USART1_init(uint32_t baudrate){
-	
-	/* This is a concept that has to do with the libraries provided by ST
-	 * to make development easier the have made up something similar to 
-	 * classes, called TypeDefs, which actually just define the common
-	 * parameters that every peripheral needs to work correctly
-	 * 
-	 * They make our life easier because we don't have to mess around with 
-	 * the low level stuff of setting bits in the correct registers
-	 */
 	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
 	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initilization
 	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
 	
-	/* enable APB2 peripheral clock for USART1 
-	 * note that only USART1 and USART6 are connected to APB2
-	 * the other USARTs are connected to APB1
-	 */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	
-	/* enable the peripheral clock for the pins used by 
-	 * USART1, PB6 for TX and PB7 for RX
-	 */
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	
-	/* This sequence sets up the TX and RX pins 
-	 * so they work correctly with the USART1 peripheral
-	 */
+
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins 6 (TX) and 7 (RX) are used
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; 			// the pins are configured as alternate function so the USART peripheral has access to them
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// this defines the IO speed and has nothing to do with the baudrate!
@@ -138,16 +83,9 @@ void USART1_init(uint32_t baudrate){
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// this activates the pullup resistors on the IO pins
 	GPIO_Init(GPIOB, &GPIO_InitStruct);					// now all the values are passed to the GPIO_Init() function which sets the GPIO registers
 
-	/* The RX and TX pins are now connected to their AF
-	 * so that the USART1 can take over control of the 
-	 * pins
-	 */
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1); //
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1); // TX
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1); // RX
 	
-	/* Now the USART_InitStruct is used to define the 
-	 * properties of USART1 
-	 */
 	USART_InitStruct.USART_BaudRate = baudrate;				// the baudrate is set to the value we passed into this init function
 	USART_InitStruct.USART_WordLength = USART_WordLength_8b;// we want the data frame size to be 8 bits (standard)
 	USART_InitStruct.USART_StopBits = USART_StopBits_1;		// we want 1 stop bit (standard)
@@ -156,12 +94,6 @@ void USART1_init(uint32_t baudrate){
 	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // we want to enable the transmitter and the receiver
 	USART_Init(USART1, &USART_InitStruct);					// again all the properties are passed to the USART_Init function which takes care of all the bit setting
 	
-	
-	/* Here the USART1 receive interrupt is enabled
-	 * and the interrupt controller is configured 
-	 * to jump to the USART1_IRQHandler() function
-	 * if the USART1 receive interrupt occurs
-	 */
 	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // enable the USART1 receive interrupt 
 	
 	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;		 // we want to configure the USART1 interrupts
@@ -174,38 +106,14 @@ void USART1_init(uint32_t baudrate){
 	USART_Cmd(USART1, ENABLE);
 }
 
-/* This funcion initializes the USART1 peripheral
- * 
- * Arguments: baudrate --> the baudrate at which the USART is 
- * 						   supposed to operate
- */
 void USART2_init(uint32_t baudrate){
-	
-	/* This is a concept that has to do with the libraries provided by ST
-	 * to make development easier the have made up something similar to 
-	 * classes, called TypeDefs, which actually just define the common
-	 * parameters that every peripheral needs to work correctly
-	 * 
-	 * They make our life easier because we don't have to mess around with 
-	 * the low level stuff of setting bits in the correct registers
-	 */
 	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
 	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initilization
 	
-	/* enable APB2 peripheral clock for USART1 
-	 * note that only USART1 and USART6 are connected to APB2
-	 * the other USARTs are connected to APB1
-	 */
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE); 
-	
-	/* enable the peripheral clock for the pins used by 
-	 * USART1, PB6 for TX and PB7 for RX
-	 */
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	
-	/* This sequence sets up the TX and RX pins 
-	 * so they work correctly with the USART1 peripheral
-	 */
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2; // Pins 6 (TX) and 7 (RX) are used
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; 			// the pins are configured as alternate function so the USART peripheral has access to them
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// this defines the IO speed and has nothing to do with the baudrate!
@@ -213,14 +121,8 @@ void USART2_init(uint32_t baudrate){
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// this activates the pullup resistors on the IO pins
 	GPIO_Init(GPIOA, &GPIO_InitStruct);					// now all the values are passed to the GPIO_Init() function which sets the GPIO registers
 
-	/* The RX and TX pins are now connected to their AF
-	 * so that the USART1 can take over control of the 
-	 * pins
-	 */
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART1); 	
-	/* Now the USART_InitStruct is used to define the 
-	 * properties of USART1 
-	 */
+
 	USART_InitStruct.USART_BaudRate = baudrate;				// the baudrate is set to the value we passed into this init function
 	USART_InitStruct.USART_WordLength = USART_WordLength_8b;// we want the data frame size to be 8 bits (standard)
 	USART_InitStruct.USART_StopBits = USART_StopBits_1;		// we want 1 stop bit (standard)
@@ -237,30 +139,21 @@ void I2C1_init(void){
 	
 	GPIO_InitTypeDef GPIO_InitStruct;
 	I2C_InitTypeDef I2C_InitStruct;
-	
-	// enable APB1 peripheral clock for I2C1
+
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
-	// enable clock for SCL and SDA pins
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	
-	/* setup SCL and SDA pins
-	 * You can connect I2C1 to two different
-	 * pairs of pins:
-	 * 1. SCL on PB6 and SDA on PB7 
-	 * 2. SCL on PB8 and SDA on PB9
-	 */
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9; // we are going to use PB8 and PB9
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;			// set pins to alternate function
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// set GPIO speed
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;			// set output to open drain --> the line has to be only pulled low, not driven high
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// enable pull up resistors
 	GPIO_Init(GPIOB, &GPIO_InitStruct);					// init GPIOB
-	
-	// Connect I2C1 pins to AF  
+
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);	// SCL
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1); // SDA
 	
-	// configure I2C1 
 	I2C_InitStruct.I2C_ClockSpeed = 100000; 		// 100kHz
 	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;			// I2C mode
 	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;	// 50% duty cycle --> standard
@@ -273,19 +166,6 @@ void I2C1_init(void){
 	I2C_Cmd(I2C1, ENABLE);
 }
 
-/* This function is used to transmit a string of characters via 
- * the USART specified in USARTx.
- * 
- * It takes two arguments: USARTx --> can be any of the USARTs e.g. USART1, USART2 etc.
- * 						   (volatile) char *s is the string you want to send
- * 
- * Note: The string has to be passed to the function as a pointer because
- * 		 the compiler doesn't know the 'string' data type. In standard
- * 		 C a string is just an array of characters
- * 
- * Note 2: At the moment it takes a volatile char because the received_string variable
- * 		   declared as volatile char --> otherwise the compiler will spit out warnings
- * */
 void USART_puts(USART_TypeDef* USARTx, volatile char *s){
 
 	while(*s){
